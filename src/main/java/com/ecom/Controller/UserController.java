83719f0189885797e8a980ddbdd4db0ea6ada722
Mohamedhusseind <34773142+Mohamedhusseind@users.noreply.github.com>
2020-04-18 21:11:52 +0200

    editing
    edit the controller for be suitable for authentication

--- src/main/java/com/ecom/Controller/UserController.java
+++ src/main/java/com/ecom/Controller/UserController.java
@@ -1,5 +1,6 @@
 package com.ecom.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecom.model.User;
@@ -16,12 +18,15 @@ public class UserController {
 	
 	@Autowired
 	private userServices userService; 
+	//List <User> alluser =new ArrayList<User>();
+	User user=new User();
 	
-	@GetMapping("/users")
-public List<User> getAllUsers()
-{
+	@GetMapping("/admin/all/user")
+	public List<User> getAllUsers()
+	{
 		return userService.getAllusers();
-}
+		 
+	}
 	@GetMapping("/user/{id}")
 	public User getUserById(@PathVariable Long id) 
 	{
